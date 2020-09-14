d6b43e323fe05012b1b06aae02974a0251cb2311
Rohan <rohan@coinzoom.com>
2020-09-14 16:07:23 +1200

    force jackson to use fields directly

--- service/src/main/java/org/dreab8/ObjectMapperContextResolver.java
+++ service/src/main/java/org/dreab8/ObjectMapperContextResolver.java
@@ -1,5 +1,7 @@
 package org.dreab8;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.ws.rs.ext.ContextResolver;
@@ -11,6 +13,10 @@ public class ObjectMapperContextResolver implements ContextResolver<ObjectMapper
 	@Override
 	public ObjectMapper getContext(Class<?> type) {
 		ObjectMapper mapper = new ObjectMapper();
+
+		mapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
+		mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+
 		return mapper;
 	}
 }
